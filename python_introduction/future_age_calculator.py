3536529749efb793cba3c2939360bc23dc02379b
Moj <moj810743@gmail.com>
2024-12-15 19:07:54 +0100

    updating the file future_age_calculator by delete the space in the end and deleting the second commande

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,4 @@
-age = input('How old are you ?') #give the user the hand to input their current age / haa l3aar la ma khadmi 
-current_age = int(age)
-age_2050 = current_age + 27 # calculate the age f the user in the year 2050
+age = int(input('How old are you?')) #give the user the hand to input their current age / haa l3aar lama khadmi 
+age_2050 = age + 27 # calculate the age f the user in the year 2050
 print("In 2050, you will be", age_2050,"years old.") # give the user their age in 2050
 
