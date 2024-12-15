8be8d535db3741dce848d420e925e878a78b8d3d
Moj <moj810743@gmail.com>
2024-12-15 17:17:12 +0100

    adding the modif on the python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 # init the firt value 
+number2 = 5  # init the second value 
+add_op = number1 + number2 # adding number1 to number2
+sub_op = number1 - number2 # subtraction opration on number1 and number2
+mult_op = number1 * number2 # multiplication ofthe two numbers 1 and 2 
+print("Addition of 10 and 5 is", add_op) 
+print("Subtraction of 10 and 5 is" , sub_op)
+print("Multiplication of 10 and 5 is" , mult_op)
+
