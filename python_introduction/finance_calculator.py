7251ab339dd5e4cd7f72f1b6ad94d7c13dbfe78c
Moj <moj810743@gmail.com>
2024-12-15 19:29:27 +0100

    fixing the error the user dont have the output that asck him about the monthly income

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,4 +1,4 @@
-monthly_income = float(ipunt('Enter your monthly income:'))
+monthly_income = float(input('Enter your monthly income:'))
 monthly_expenses = float(input('Enter your monthly expenses'))
 monthly_savings = monthly_income - monthly_expenses
 annual_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
