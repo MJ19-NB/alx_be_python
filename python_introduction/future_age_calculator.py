2b22e6a332285d2caaaf7073abd48ecdc178414e
Moj <moj810743@gmail.com>
2024-12-15 18:31:31 +0100

    fixig the error for te second time about the question how ask the user about the current age

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = int(input("How old are you ? ")) #give the user the hand to input their current age
+age = input("How old are you ? ") #give the user the hand to input their current age
 current_age = int(age)
 age_2050 = current_age + 27 # calculate the age f the user in the year 2050
 print("In 2050, you will be", age_2050,"years old.") # give the user their age in 2050
