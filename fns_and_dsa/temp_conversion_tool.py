5c432de609da86b9b078e5615e74223c1d065e93
Moj <moj810743@gmail.com>
2025-01-20 14:48:50 +0100

    correcting the file temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,7 @@
 # Part 1: Define Global Conversion Factors
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Conversion factor from Celsius to Fahrenheit
-
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Conversion factor from Fahrenheit to Celsius
-FREEZING_POINT_FAHRENHEIT = 32        # Freezing point of water in Fahrenheit
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FREEZING_POINT_FAHRENHEIT = 32
 
 # Part 2: Conversion Functions
 def convert_to_celsius(fahrenheit):
